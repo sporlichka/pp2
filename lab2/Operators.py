ac8034e371bd27336ff135665ef4812ffc934ca8
sporlichka <157012180+sporlichka@users.noreply.github.com>
2024-01-30 11:06:58 +0600

    lab2

---
+++ lab2/Operators.py
@@ -0,0 +1,28 @@
+print(10*5)
+##50
+#
+print(10/2)
+## 5
+#
+fruits = ["apple", "banana"]
+if "apple" in fruits:
+  print("Yes, apple is a fruit!")
+## Yes and etc
+#
+if 5 != 10:
+  print("5 and 10 is not equal")
+#
+if 5==10 or 4==4:
+   print("At least one of the statements is true")
+#
+fruits = ["apple", "banana", "cherry"]
+print(fruits[1])
+## banana
+#
+fruits = ["apple", "banana", "cherry"]
+fruits[0] = "kiwi"
+## apple changed to kiwi
+#
+fruits = ["apple", "banana", "cherry"]
+fruits.append("orange")
+print(fruits)
